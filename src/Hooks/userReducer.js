ebe72065b98b41449cf1d6264ab7668b97073763
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-07-12 15:07:12 +0530

    Use Reducer React

--- src/Hooks/userReducer.js
+++ src/Hooks/userReducer.js
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useReducer } from "react";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Incremeant":
+      return { count: state.count + 1, showtext: state.showtext };
+    case "Toggle":
+      return { count: state.count, showtext: !state.showtext };
+    default:
+      return state;
+  }
+};
 const UseReducer = () => {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, { count: 0, showtext: false });
+  return (
+    <div>
+      <h3>{state.count}</h3>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "Incremeant" });
+          dispatch({ type: "Toggle" });
+        }}
+      >
+        Click Me
+      </button>
+      {state.showtext && <p>This is a text</p>}
+    </div>
+  );
 };
 
 export default UseReducer;
