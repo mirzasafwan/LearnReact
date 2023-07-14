8c2f5c00fa658069c65bf21c1dd79d2457f729e5
mirzasafwan <mohammedsafwanmirza786@gmail.com>
2023-07-14 14:31:01 +0530

    More Functionality

--- src/Hooks/useEffect.js
+++ src/Hooks/useEffect.js
@@ -11,10 +11,20 @@ function UseEffect() {
       .then((response) => {
         return setData(response.data[0]);
       });
-  }, []);
+    setCount(count);
+    // setTimeout(() => {
+    //   setCount(count + 1);
+    // }, 1000);
+  }, [count]); // Never to pass array at the end []
+  //   useEffect(() => {
+  //     setTimeout(() => {
+  //       setCount(count + 1);
+  //     }, 1000);
+  //   }, [count]);
 
   return (
     <div>
+      <h3>{count}</h3>
       <strong>id: </strong> {data.id}
     </div>
   );
